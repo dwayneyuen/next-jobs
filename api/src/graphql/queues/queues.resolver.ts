4a01737929725c82be412264301adab1be192919
Dwayne Yuen <dwayneyuen@gmail.com>
2022-06-12 16:14:53 -0700

    refactor: delete job classes

--- api/src/graphql/queues/queues.resolver.ts
+++
@@ -1,5 +0,0 @@
-import { Resolver } from "@nestjs/graphql";
-import { QueueModel } from "src/graphql/queues/queue.model";
-
-@Resolver(() => QueueModel)
-export class QueuesResolver {}
--- api/src/graphql/scheduled-jobs/scheduled-jobs.resolver.ts
+++
@@ -1,5 +0,0 @@
-import { Resolver } from "@nestjs/graphql";
-import { ScheduledJobModel } from "src/graphql/scheduled-jobs/scheduled-job.model";
-
-@Resolver(() => ScheduledJobModel)
-export class ScheduledJobsResolver {}
