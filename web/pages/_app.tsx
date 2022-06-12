d69d38d913d56374049aa56a1d1c05b5640072da
Dwayne Yuen <dwayneyuen@gmail.com>
2022-06-11 20:13:52 -0700

    fix: fix import

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import { ApolloProvider } from "@apollo/client";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-import apolloClient from "apollo-client";
+import apolloClient from "lib/apollo-client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
